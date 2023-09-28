56bb57e32e6385d3e5e5a275042cfcb7c6e8af7c
Tazzy2022 <micheletazbaz@gmail.com>
2023-09-27 18:17:34 -0700

    still broken but merging with main

--- app/components/UserUpdateGenrePref.js
+++ app/components/UserUpdateGenrePref.js
@@ -52,13 +52,8 @@ export default function UserUpdateGenrePref() {
 
   const userGenres = auth.userGenrePrefs;
 
-  console.log("GENRES", genres);
-  console.log("userGenres", userGenres);
-
   const populateCheckboxes = () => {
     if (userGenres?.length > 0) {
-      // checked.forEach((genre) => {
-
       userGenres.forEach((userPref) => {
         const nextCheck = [...checked];
         const found = nextCheck.find((g) => {
@@ -66,7 +61,6 @@ export default function UserUpdateGenrePref() {
         });
         found.checked = true;
         setCheckedState(nextCheck);
-
       });
     }
   };
@@ -74,7 +68,7 @@ export default function UserUpdateGenrePref() {
   const handleChange = (e) => {
     setCheckedState((prevState) => [
       ...prevState,
-      { ...genre, genreTmdbId: e.target.value, checked: e.target.checked },
+      { ...checked, genreTmdbId: e.target.value, checked: e.target.checked },
     ]);
     //console.log("checked", checked, "e.target.value", e.target.value);
   };
@@ -93,102 +87,30 @@ export default function UserUpdateGenrePref() {
     <div className="usergenrepref-container-margin">
       <p className="p-heading">UPDATE YOUR GENRE PREFERENCES:</p>
       <div></div>
-      <p>Please select at least 2 genres you enjoy!</p>
-      <Box component="form" onSubmit={handleSubmit} sx={{ display: "flex" }}>
-        <FormControl sx={{ marginBottom: "1rem" }}>
+      <p className="genre-p">
+        Please make sure at least 2 genres are selected!
+      </p>
+      <Box
+        className="genre-form"
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{ display: "flex" }}
+      >
+        <FormControl sx={{ marginBottom: "1rem", display: "flex" }}>
           <CopyGenreCheckboxes
             selectedGenres={checked}
             handleChange={handleChange}
           />
-          {/* <FormHelperText
-            // error={genreError}
-            sx={{ marginLeft: "2rem", marginTop: "-0.5rem" }}
-          >
-            Please select at least 2 genres you enjoy!
-          </FormHelperText> */}
         </FormControl>
-        <div></div>
-        <Button variant="contained" color="success" type="submit">
+        <Button
+          className="btn"
+          variant="contained"
+          color="success"
+          type="submit"
+        >
           update
         </Button>
       </Box>
     </div>
   );
 }
-// export default function UserUpdateGenrePref() {
-//   const auth = useSelector((state) => state.auth);
-
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     dispatch(
-//       getUserGenrePrefs({
-//         id: auth.user.id,
-//         token: auth.token,
-//       })
-//     );
-//   }, []);
-
-//   useEffect(() => {
-//     populateCheckboxes();
-//   }, [auth.userGenrePrefs]);
-
-//   const [checked, setCheckedState] = React.useState({});
-//   //const [data, setData] = React.useState([]);
-//   const [data, setData] = React.useState([]);
-
-//   const userGenres = auth.userGenrePrefs;
-
-//   const populateCheckboxes = () => {
-//     if (userGenres?.length > 0 && !checked.length) {
-//       userGenres?.filter((genre) => {
-//         setData((prevProps) => {
-//           return { ...prevProps, [genre.genreTmdbId]: true };
-//         });
-//       });
-//     }
-//   };
-
-//   // {id: true}
-//   const handleChange = (e) => {
-//     setCheckedState(() => ({
-//       ...data,
-//       [e.target.value]: e.target.checked,
-//     }));
-//     console.log("checked", checked, "e.target.value", e.target.value);
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     for (let key in checked) {
-//       setData(parseInt(key));
-//     }
-//     if (data.length < 2) {
-//       window.alert("please check at least 2 genres");
-//     }
-//     console.log("data in submit", data);
-//   };
-//   return (
-//     <div className="usergenrepref-container-margin">
-//       <p className="p-heading">UPDATE YOUR GENRE PREFERENCES:</p>
-//       <p>Please select at least 2 genres you enjoy!</p>
-//       <Box component="form" onSubmit={handleSubmit} sx={{ display: "flex" }}>
-//         <FormControl sx={{ marginBottom: "1rem" }}>
-//           <CopyGenreCheckboxes
-//             selectedGenres={data}
-//             handleChange={handleChange}
-//           ></CopyGenreCheckboxes>
-//           {/* <FormHelperText
-//             // error={genreError}
-//             sx={{ marginLeft: "2rem", marginTop: "-0.5rem" }}
-//           >
-//             Please select at least 2 genres you enjoy!
-//           </FormHelperText> */}
-//         </FormControl>
-//         <Button variant="contained" color="success" type="submit">
-//           update
-//         </Button>
-//       </Box>
-//     </div>
-//   );
-//}
