278fb6e56fffdc126b6069eefba327d388f30f08
Kevin Choi <choi1june88@gmail.com>
2023-09-27 19:21:28 -0700

    fixing deploy3

--- server/db/db.js
+++ server/db/db.js
@@ -8,7 +8,8 @@ if (process.env.QUIET) {
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost/watchifydb",
+  process.env.DATABASE_URL ||
+    "postgres://watchify_2k9k_user:HP5ZED4tIGg0ifWF2PY6XVyMJTZSHUJu@dpg-ckae4nmgtj9c73fhm48g-a.oregon-postgres.render.com/watchify_2k9k",
   { logging: false }
 );
 
