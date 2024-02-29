25b2c7f0374bd193b1fa144d75da09dac77f793c
Choudhury Yusuf <126734019+spikey2023@users.noreply.github.com>
2024-02-29 11:47:30 -0700

    Updated landing page styling and added background image

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./Hero.css";
 
-import heroimg from "../../public/heroimg.jpg";
+import bg1 from "../../public/bg1.jpg"
+import bg2 from "../../public/bg2.jpg"
+import bg3 from "../../public/bg3.jpg"
+import bg4 from "../../public/bg4.jpg"
+import bg5 from "../../public/bg5.jpeg"
+import bg6 from "../../public/bg6.jpeg"
 
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const Hero = () => {
+  let bg_imgs = [bg1, bg2, bg3, bg4, bg5, bg6]
+  let bg_img = bg_imgs[Math.floor(Math.random()*bg_imgs.length)]
   return (
+    <>
+              <div className="backdrop-img">
+            <img src={bg_img} alt="Loading" />
+          </div>
+          <div className="opacity-layer"> </div>
+
     <div className="container">
-      <img src={heroimg} alt="Loading" />
+      {/* <img src={heroimg} alt="Loading" /> */}
       <div className="hero-text">
-  
-        <h1> Watchify - Your Personal Movie Universe!</h1>
+        <h1> 
+          <span> WATCHIFY</span> - Your Personal Movie Universe!</h1>
         <p>Say goodbye to endless browsing on your Friday nights!</p>
         <p>
           Get Personalized Movie Recommendations to Watch Online Based on Your
@@ -37,6 +50,7 @@ const Hero = () => {
         </motion.div>
       </div>
     </div>
+    </>
   );
 };
 
